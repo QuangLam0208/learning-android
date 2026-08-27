34dc1c43f71fa4b19aa6048791d0ba9d73258288
Luong Quang Lam <laulua1223@gmail.com>
2026-08-27 16:12:40 +0700

    add: SerializedName

--- app/src/main/java/com/persy/learnandroid/model/Todo.java
+++ app/src/main/java/com/persy/learnandroid/model/Todo.java
@@ -3,13 +3,20 @@ package com.persy.learnandroid.model;
 import androidx.room3.Entity;
 import androidx.room3.PrimaryKey;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.Date;
 
 @Entity(tableName = "todo")
 public class Todo {
     @PrimaryKey(autoGenerate = true)
-    private int id = 0;
-    private  String title;
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("createAt")
     private Date createAt;
 
     public Todo(String title, Date createAt) {
@@ -20,7 +27,6 @@ public class Todo {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -28,7 +34,6 @@ public class Todo {
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
@@ -36,7 +41,6 @@ public class Todo {
     public Date getCreateAt() {
         return createAt;
     }
-
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
     }
