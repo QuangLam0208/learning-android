5d734e7deec5a584c741aaf57a171db5a7f545ee
Luong Quang Lam <laulua1223@gmail.com>
2026-08-27 18:34:18 +0700

    rename todo retrofit

--- app/src/main/java/com/persy/learnandroid/api/TodoRetrofitClient.java
+++ app/src/main/java/com/persy/learnandroid/api/TodoRetrofitClient.java
@@ -12,7 +12,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitClient {
+public class TodoRetrofitClient {
 
     private static final String BASE_URL = "http://10.0.2.2:8090/api/";
     private static Retrofit retrofit;
