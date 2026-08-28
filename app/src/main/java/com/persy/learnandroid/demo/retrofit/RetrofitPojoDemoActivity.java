02887f5babb1380e10d96fb962ea40237411feba
Luong Quang Lam <laulua1223@gmail.com>
2026-08-29 00:37:30 +0700

    use binding

--- app/src/main/java/com/persy/learnandroid/demo/retrofit/RetrofitPojoDemoActivity.java
+++ app/src/main/java/com/persy/learnandroid/demo/retrofit/RetrofitPojoDemoActivity.java
@@ -1,57 +1,43 @@
 package com.persy.learnandroid.demo.retrofit;
 
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.persy.learnandroid.R;
+import com.persy.learnandroid.databinding.ActivityRetrofitPojoBinding;
 
 public class RetrofitPojoDemoActivity extends AppCompatActivity {
 
-    private TextView tvExplanation;
-    private Toolbar toolbar;
+    private ActivityRetrofitPojoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_retrofit_pojo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_retrofit_pojo);
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        tvExplanation = findViewById(R.id.tvExplanation);
-        toolbar = findViewById(R.id.toolbar);
-
         setupToolBar();
-        setTextExplanation();
     }
 
     private void setupToolBar() {
-        setSupportActionBar(toolbar);
+        setSupportActionBar(binding.includeToolbar.toolbar);
         if (getSupportActionBar() != null) {
             String topicTitle = getIntent().getStringExtra("EXTRA_TOPIC_TITLE");
-            getSupportActionBar().setTitle(topicTitle);
+            getSupportActionBar().setTitle(topicTitle != null ? topicTitle : "Retrofit POJO");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        toolbar.setNavigationOnClickListener(view -> finish());
-    }
-
-    private void setTextExplanation() {
-        tvExplanation.setText(
-                Html.fromHtml(
-                        getString(R.string.retrofit_pojo_explanation),
-                        Html.FROM_HTML_MODE_LEGACY
-                )
-        );
+        binding.includeToolbar.toolbar.setNavigationOnClickListener(view -> finish());
     }
 }
